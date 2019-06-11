928789ce130a35dd6ce79a4aa6fc95875e81bd6a
rencro <rencro3@gmail.com>
2019-06-11 16:01:36 -0700

    Video 15: New Quit button

--- SFMLTesting/MainMenuState.cpp
+++ SFMLTesting/MainMenuState.cpp
@@ -26,14 +26,24 @@ void MainMenuState::initKeybinds()
 	ifs.close();
 }
 
+void MainMenuState::initButtons()
+{
+	this->buttons["GAME_STATE"] = new Button(100, 100, 150, 50, &this->font, "New Game",
+		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 255), 
+		sf::Color(20, 20, 20, 200));
+
+	this->buttons["EXIT_STATE"] = new Button(100, 300, 150, 50, &this->font, "Quit",
+		sf::Color(100, 100, 100, 200), sf::Color(150, 150, 150, 255),
+		sf::Color(20, 20, 20, 200));
+
+}
+
 MainMenuState::MainMenuState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys)
 	: State(window, supportedKeys)
 {
 	this->initFonts();
 	this->initKeybinds();
-
-	this->gamestate_btn = new Button(100, 100, 150, 50, &this->font, "New Game",
-		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 255), sf::Color(20, 20, 20, 200));
+	this->initButtons();
 
 	this->background.setSize(sf::Vector2f(window->getSize().x, window->getSize().y));
 	this->background.setFillColor(sf::Color::Magenta);
@@ -41,7 +51,11 @@ MainMenuState::MainMenuState(sf::RenderWindow* window, std::map<std::string, int
 
 MainMenuState::~MainMenuState()
 {
-	delete this->gamestate_btn;
+	auto it = this->buttons.begin();
+	for (it = this->buttons.begin(); it != this->buttons.end(); ++it)
+	{
+		delete it->second;
+	}
 }
 
 void MainMenuState::endState()
@@ -56,12 +70,42 @@ void MainMenuState::updateInput(const float& dt)
 	//update player input
 }
 
+void MainMenuState::updateButtons()
+{
+	//Updates all the buttons in the state and handles their functionality.
+	for (auto& it : this->buttons)
+	{
+		it.second->update(this->mousePosView);
+	}
+
+	//New game
+	if (this->buttons["GAME_STATE"]->isPressed())
+	{
+		//this->states.push(new MainMenuState(this->window, &this->supportedKeys));
+	}
+
+	//Quit the game
+	if (this->buttons["EXIT_STATE"]->isPressed())
+	{
+		this->quit = true;
+	}
+}
+
 void MainMenuState::update(const float& dt)
 {
 	this->updateMousePositions();
 	this->updateInput(dt);
 
-	this->gamestate_btn->update(this->mousePosView);
+	this->updateButtons();
+}
+
+void MainMenuState::renderButtons(sf::RenderTarget* target)
+{
+	for (auto& it : this->buttons)
+	{
+		it.second->render(target);
+	}
+
 }
 
 void MainMenuState::render(sf::RenderTarget* target)
@@ -71,5 +115,5 @@ void MainMenuState::render(sf::RenderTarget* target)
 
 	target->draw(this->background);
 
-	this->gamestate_btn->render(target);
+	this->renderButtons(target);
 }
--- SFMLTesting/MainMenuState.h
+++ SFMLTesting/MainMenuState.h
@@ -12,11 +12,12 @@ private:
 	sf::RectangleShape background;
 	sf::Font font;
 
-	Button* gamestate_btn;
+	std::map<std::string, Button*> buttons;
 
 	//Functions
 	void initFonts();
 	void initKeybinds();
+	void initButtons();
 
 public:
 	MainMenuState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys);
@@ -26,7 +27,9 @@ public:
 	void endState();
 
 	void updateInput(const float& dt);
+	void updateButtons();
 	void update(const float& dt);
+	void renderButtons(sf::RenderTarget* target = nullptr);
 	void render(sf::RenderTarget* target = nullptr);
 
 };
