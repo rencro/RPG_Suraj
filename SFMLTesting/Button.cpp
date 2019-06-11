22bb00c1a6c15622c42b654b8f6f243722e1d0e4
rencro <rencro3@gmail.com>
2019-06-10 17:22:46 -0700

    Video 13: Button class

--- SFMLTesting/Button.cpp
+++ SFMLTesting/Button.cpp
@@ -1,11 +1,46 @@
 #include "Button.h"
 
-Button::Button()
+Button::Button(float x, float y, float width, float height, sf::Font* font, std::string text,
+	sf::Color idleColor, sf::Color hoverColor, sf::Color activeColor)
 {
+	this->shape.setPosition(sf::Vector2f(x, y));
+	this->shape.setSize(sf::Vector2f(width, height));
+	this->font = font;
+	this->text.setFont(*this->font);
+	this->text.setString(text);
+	this->text.setFillColor(sf::Color::White);
+	this->text.setCharacterSize(12);
+	this->text.setPosition(
+		this->shape.getPosition().x / 2.f - this->text.getGlobalBounds().width / 2.f,
+		this->shape.getPosition().y / 2.f - this->text.getGlobalBounds().height / 2.f
+	);
+
+
+
+	this->idleColor = idleColor;
+	this->hoverColor = hoverColor;
+	this->activeColor = activeColor;
+
+	this->shape.setFillColor(this->idleColor);
 
 }
 
 Button::~Button()
 {
 
+}
+
+void Button::update(const sf::Vector2f mousePos)
+{
+	//Update the boolean for hover and pressed
+
+	if (this->shape.getGlobalBounds().contains(mousePos))
+	{
+
+	}
+}
+
+void Button::render(sf::RenderTarget* target)
+{
+	target->draw(this->shape);
 }
--- SFMLTesting/Button.h
+++ SFMLTesting/Button.h
@@ -1,13 +1,38 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\Audio.hpp>
+
+#include <iostream>
+#include <ctime>
+#include <sstream>
+
+
+
+
 class Button
 {
 private:
+	sf::RectangleShape shape;
+	sf::Font* font;
+	sf::Text text;
+
+	sf::Color idleColor;
+	sf::Color hoverColor;
+	sf::Color activeColor;
 
 public:
-	Button();
+	Button(float x, float y, float width, float height, sf::Font* font, std::string text, 
+		sf::Color idleColor, sf::Color hoverColor, sf::Color activeColor);
 	~Button();
+
+	//Functions
+	void update(const sf::Vector2f mousePos);
+	void render(sf::RenderTarget* target);
+
 };
 
 #endif
